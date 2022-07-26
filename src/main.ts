992d4c045648d0febd0ca3a34e6955af2e99d26d
Vadim <vadim.setsko@celadonsoft.com>
2022-07-26 14:49:47 +0300

    Heroku

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,8 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(5001);
+  await app.listen(process.env.PORT || 5001, () => {
+    console.log(`Server running on port ${process.env.PORT || 5001}`);
+  });
 }
 bootstrap();
