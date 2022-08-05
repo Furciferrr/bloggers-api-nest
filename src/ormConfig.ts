8b1f21b9ba82acfec98471a2893d66f99e04d847
Vadim <vadim.setsko@celadonsoft.com>
2022-08-05 11:22:58 +0300

    Heroku

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -2,13 +2,14 @@ import { DataSourceOptions } from 'typeorm';
 
 const ormConfig: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '123456',
-  database: 'bloggers',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: +process.env.POSTGRES_PORT || 5432,
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_DB_PASSWORD || '123456',
+  database: process.env.POSTGRES_DATABASE || 'bloggers',
   entities: [__dirname + '/**/**/*.entity{.ts,.js}'],
-  synchronize: true,
+  synchronize: false,
 };
 
 export default ormConfig;
+
