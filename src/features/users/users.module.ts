9d903a8cd14d9c8df53f8361980cda22d66f7603
Vadim <vadim.setsko@celadonsoft.com>
2022-08-15 12:58:42 +0300

    Heroku

--- src/features/users/users.module.ts
+++ src/features/users/users.module.ts
@@ -14,6 +14,7 @@ import { UserSQLRepository } from './usersSQL.repository';
 @Module({
   imports: [
     //MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    
     TypeOrmModule.forFeature([UserEntity]),
     ConfigModule,
   ],
